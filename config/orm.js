9680e9eaa772f440c7ab9fc1dc35fe70ea87faf4
Kelly Jiang <kllyjiang@gmail.com>
2019-11-01 23:23:11 -0500

    Adding greater generality to orm

--- config/orm.js
+++ config/orm.js
@@ -1,23 +1,32 @@
 const connection = require("./connection.js");
 
 const orm = {
-    selectAll: function(callback) {
-        connection.query("SELECT * FROM burgers", (err,result) => {
+    selectAll: function(table, callback) {
+        connection.query("SELECT * FROM ??", [table], (err,result) => {
             if(err) throw err;
         });
         callback(result);
     },
-    insertOne: function(name, callback) {
-        connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [name], (err,result) => {
+    insertOne: function(table, columns, values, callback) {
+        let queryString = "INSERT INTO ?? (";
+        for(i = 0; i < columns.length-1; i++) {
+            queryString += "?,";
+        }
+        queryString += "?) VALUES (";
+        for(i = 0; i < values.length-1; i++) {
+            queryString += "?,";
+        }
+        queryString += "?)";
+        connection.query(queryString, [table].concat(columns).concat(values), (err,result) => {
             if(err) throw err;
+            callback(result);
         });
-        callback(result);
     },
-    updateOne: function(id, callback) {
-        connection.query("UPDATE burgers SET devoured = NOT devoured WHERE id = ?", [id], (err,result) => {
+    updateOne: function(table, updateCol, updateVal, id, callback) {
+        connection.query("UPDATE ? SET ? = ? WHERE id = ?", [table, updateCol, updateVal, id], (err,result) => {
             if(err) throw err;
+            callback(result);
         });
-        callback(result);
     }
 };
 
