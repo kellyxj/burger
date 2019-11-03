17dd972c65e8c1aa8b403e2b19496ace12242311
Kelly Jiang <kllyjiang@gmail.com>
2019-11-02 21:25:13 -0500

    Using ?? instead of ? for column names

--- config/orm.js
+++ config/orm.js
@@ -2,31 +2,39 @@ const connection = require("./connection.js");
 
 const orm = {
     selectAll: function(table, callback) {
-        connection.query("SELECT * FROM ??", [table], (err,result) => {
-            if(err) throw err;
+        connection.query("SELECT * FROM ??", [table], (err, result) => {
+            if (err) throw err;
+            callback(result);
         });
-        callback(result);
     },
     insertOne: function(table, columns, values, callback) {
         let queryString = "INSERT INTO ?? (";
-        for(i = 0; i < columns.length-1; i++) {
-            queryString += "?,";
+        for (i = 0; i < columns.length - 1; i++) {
+            queryString += "??,";
         }
-        queryString += "?) VALUES (";
-        for(i = 0; i < values.length-1; i++) {
+        queryString += "??) VALUES (";
+        for (i = 0; i < values.length - 1; i++) {
             queryString += "?,";
         }
         queryString += "?)";
-        connection.query(queryString, [table].concat(columns).concat(values), (err,result) => {
-            if(err) throw err;
-            callback(result);
-        });
+        connection.query(
+            queryString,
+            [table].concat(columns).concat(values),
+            (err, result) => {
+                if (err) throw err;
+                callback(result);
+            }
+        );
     },
     updateOne: function(table, updateCol, updateVal, id, callback) {
-        connection.query("UPDATE ? SET ? = ? WHERE id = ?", [table, updateCol, updateVal, id], (err,result) => {
-            if(err) throw err;
-            callback(result);
-        });
+        connection.query(
+            "UPDATE ?? SET ?? = ? WHERE id = ?",
+            [table, updateCol, updateVal, id],
+            (err, result) => {
+                if (err) throw err;
+                callback(result);
+            }
+        );
     }
 };
 
