cf4a6a53e4ee3e4d42df3403d6d58debc34c7c49
Kelly Jiang <kllyjiang@gmail.com>
2019-12-07 14:56:37 -0600

    Changing to remote database

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(30) NOT NULL,
     devoured BOOLEAN NOT NULL DEFAULT 0,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
 
--- models/burger.js
+++ models/burger.js
@@ -2,13 +2,15 @@ const orm = require("../config/orm.js");
 
 const burger = {
     all: function(callback) {
-        orm.selectAll("burgers", res => callback(res));
+        orm.selectAll("c0imvegq7bn8ug3p", res => callback(res));
     },
     create: function(name, callback) {
-        orm.insertOne("burgers", ["burger_name"], [name], res => callback(res));
+        orm.insertOne("c0imvegq7bn8ug3p", ["burger_name"], [name], res =>
+            callback(res)
+        );
     },
     update: function(updateVal, id, callback) {
-        orm.updateOne("burgers", "devoured", updateVal, id, res =>
+        orm.updateOne("c0imvegq7bn8ug3p", "devoured", updateVal, id, res =>
             callback(res)
         );
     }
