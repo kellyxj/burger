9ec0cd306cab3bb402472ca26f81e016966b3e1d
Kelly Jiang <kllyjiang@gmail.com>
2019-12-07 14:40:59 -0600

    Hooking jaws_db

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Un1c0rnZ!",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Un1c0rnZ!",
+        database: "burgers_db"
+    });
+}
 
 connection.connect(err => {
-    if(err) {
+    if (err) {
         console.error(err.stack);
         return;
     }
